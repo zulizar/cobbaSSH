ac9a86d6eb3410257c7fc80662bc9d1024709df8
zulizar <zulizar.hardi@gmail.com>
2018-01-20 15:10:53 +0700

    Update trial.sh

--- trial.sh
+++ trial.sh
@@ -13,9 +13,8 @@ useradd -e `date -d "$hari days" +"%Y-%m-%d"` -s /bin/false -M thornssh-$Login
 echo -e "$Pass\n$Pass\n"|passwd thornssh-$Login &> /dev/null
 echo -e ""
 echo -e "------------------------------- MEMBUAT AKUN SSH -------------------------------"
-echo -e "                         ALL SUPPORTED BY THORN SSH                     "
-echo -e "           https://www.facebook.com/groups/1515866931798557/?ref=bookmarks"
-echo -e "  DEVELOPED BY Yonatan Kanu (https://www.facebook.com/kanu.nugraha , 085707136028)"
+echo -e "                         ALL SUPPORTED BY RUMAHSSH SSH                     "
+
 
 echo -e "===YAH TRIALAN MANG :V==="
 echo -e "====TRIAL SSH account===="
@@ -24,13 +23,10 @@ echo -e "Port OpenSSH: 22,80"
 echo -e "Port Dropbear: 143,443"
 echo -e "Squid: 8080,3128"
 echo -e "Config OpenVPN (TCP 1194): http://$IP:81/client.ovpn"
-echo -e "Username: thornssh-$Login"
+echo -e "Username: $Login"
 echo -e "Password: $Pass\n"
 echo -e "=========================="
-echo -e "ALL SUPPORTED by Yonatan Kanu as THORN SSH"
-echo -e "Contact Person : https://www.facebook.com/kanu.nugraha"
-echo -e "WhatsApp       : 085707136028"
-echo -e "Website        : http://yonatankanu.blogspot.co.id"
-echo -e "Website        : http://yonatankanu.wordpress.com/"
+echo -e "ALL SUPPORTED by Yonatan Kanu as Rumah SSH"
+
 
 echo -e ""
